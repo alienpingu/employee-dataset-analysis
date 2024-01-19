df5a65f117b3d2769ae4efe2cab94bac69106e43
sbertocco <samuele.bertocco@soprasteria.com>
2024-01-19 09:07:43 +0100

    test - pass test for each function

--- test/core.test.ts
+++ test/core.test.ts
@@ -24,9 +24,28 @@ const mockData:Employee[]  = [
 ]
 
 describe("Core", () => {
-    describe("Initial Value Declaration", () => {
+    describe("INIT - Initial Value Declaration", () => {
         const core = new Core(mockData);
-        it("Should return the dataset", () => expect(core.dataset).toStrictEqual(mockData))
-        it("Should return the dataset", () => expect(core.departments).toStrictEqual(['HR','Marketing']))
+        it("Should return initial dataset", () => expect(core.dataset).toStrictEqual(mockData))
+    })
+    describe("generateDepartment - First element must be HR obj", () => {
+        const core = new Core(mockData);
+        it("Should return name of first department (=HR)", () => expect(core.departments[0].department).toStrictEqual('HR'))
+    })
+    describe("generateEmployee - Get list from employee by department", () => {
+        const core = new Core(mockData);
+        it("Should return array of employee", () => expect(core.departments[0].employee.length).toStrictEqual(2))
+    })
+    describe("generateSalary - Get avarage salary for department", () => {
+        const core = new Core(mockData);
+        it("Should return avarange salary from department", () => expect(core.departments[0].average_salary).not.toStrictEqual(0))
+    })
+    describe("generateYear - Get avarage years of service for department", () => {
+        const core = new Core(mockData);
+        it("Should return avarange salary from department", () => expect(core.departments[0].total_year_of_service).not.toStrictEqual(0))
+    })
+    describe("getRichest - Get richest employee for department", () => {
+        const core = new Core(mockData);
+        it("Should return employee with highest salary", () => expect(core.departments[0].richest_employee).not.toStrictEqual(null))
     })
 })
